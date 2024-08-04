67809176589b70f086f52f6f734801433ef9a043
VishnuSusilkumar <vishnususilkumar10@gmail.com>
2024-08-04 11:01:42 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { connectDB } from "./config/db";
+import RabbitMQClient from "./events/rabbitMQ/client";
+
+class App {
+  constructor() {
+    this.initializeRabbitMQ();
+    this.connectDatabase();
+  }
+
+  private initializeRabbitMQ(): void {
+    RabbitMQClient.initialize();
+  }
+
+  private connectDatabase(): void {
+    connectDB();
+  }
+}
+
+export default App;
